9397febe2c7ae51390641f25ee34e5630231428d
Thomas <teleathers22@gmail.com>
2017-09-09 11:45:09 +0800

    redesign settings utility

--- settings.py
+++ settings.py
@@ -12,6 +12,7 @@ import os
 import subprocess
 from pygame.locals import *
 import VMSYSTEM.libvmconf as libvmconf
+import VMSYSTEM.libvmui as vmui
 
 pygame.display.init()
 pygame.font.init()
@@ -20,20 +21,17 @@ pygame.display.set_caption(("Settings"), ("Settings"))
 windowicon=pygame.image.load(os.path.join(os.path.join('VMSYSTEM', 'GFX', "settings"), 'settings64.png'))
 pygame.display.set_icon(windowicon)
 
-screensurf=pygame.display.set_mode((800, 600))
-
+screensurf=pygame.display.set_mode((450, 300))
+vmui.initui(screensurf, 1)
 #image data
-setbg=pygame.image.load(os.path.join(os.path.join('VMSYSTEM', 'GFX', "settings"), 'settingsbg.jpg')).convert()
+setbg=pygame.image.load(os.path.join(os.path.join('VMSYSTEM', 'GFX', "settings"), 'settingsbg2.jpg')).convert()
 swon=pygame.image.load(os.path.join(os.path.join('VMSYSTEM', 'GFX', "settings"), 'switchon.png')).convert()
 swoff=pygame.image.load(os.path.join(os.path.join('VMSYSTEM', 'GFX', "settings"), 'switchoff.png')).convert()
-csup=pygame.image.load(os.path.join(os.path.join('VMSYSTEM', 'GFX', "settings"), 'cpusuper.png')).convert()
-csupsel=pygame.image.load(os.path.join(os.path.join('VMSYSTEM', 'GFX', "settings"), 'cpusupers.png')).convert()
-cnorm=pygame.image.load(os.path.join(os.path.join('VMSYSTEM', 'GFX', "settings"), 'cpunorm.png')).convert()
-cnormsel=pygame.image.load(os.path.join(os.path.join('VMSYSTEM', 'GFX', "settings"), 'cpunorms.png')).convert()
-cfas=pygame.image.load(os.path.join(os.path.join('VMSYSTEM', 'GFX', "settings"), 'cpufast.png')).convert()
-cfassel=pygame.image.load(os.path.join(os.path.join('VMSYSTEM', 'GFX', "settings"), 'cpufasts.png')).convert()
-cslow=pygame.image.load(os.path.join(os.path.join('VMSYSTEM', 'GFX', "settings"), 'cpuslower.png')).convert()
-cslowsel=pygame.image.load(os.path.join(os.path.join('VMSYSTEM', 'GFX', "settings"), 'cpuslowers.png')).convert()
+
+cpubtn=pygame.image.load(os.path.join(os.path.join('VMSYSTEM', 'GFX', "settings"), 'cpuspeed.png')).convert()
+mixbtn=pygame.image.load(os.path.join(os.path.join('VMSYSTEM', 'GFX', "settings"), 'mixrate.png')).convert()
+fskipbtn=pygame.image.load(os.path.join(os.path.join('VMSYSTEM', 'GFX', "settings"), 'frameskip.png')).convert()
+fvfilemenu=pygame.image.load(os.path.join(os.path.join('VMSYSTEM', 'GFX', "settings"), 'fvfilemenu.png')).convert()
 
 lbtn=pygame.image.load(os.path.join(os.path.join('VMSYSTEM', 'GFX', "settings"), 'lbtn.png')).convert()
 rbtn=pygame.image.load(os.path.join(os.path.join('VMSYSTEM', 'GFX', "settings"), 'rbtn.png')).convert()
@@ -44,7 +42,7 @@ helpicn=pygame.image.load(os.path.join(os.path.join('VMSYSTEM', 'GFX', "settings
 
 simplefontC = pygame.font.SysFont(None, 28)
 simplefontB = pygame.font.SysFont(None, 19)
-
+catfont = pygame.font.SysFont(None, 19)
 
 
 CPUWAIT=libvmconf.getconf("cpu", "cpuwait")#
@@ -61,6 +59,13 @@ ttystyle=int(libvmconf.getconf("video", "ttystyle"))#
 #DEFAULTSTREG=libvmconf.getconf("bootup", "DEFAULTSTREG")
 mixrate=int(libvmconf.getconf("audio", "mixrate"))#
 
+fm0=vmui.menuitem("Help (F1)", "HELP")
+fm1=vmui.menuitem("Save", "SAVE")
+fm2=vmui.menuitem("Reset", "RESET")
+fm3=vmui.menuitem("Quit", "QUIT")
+filemenu=[fm0, fm1, fm2, fm3]
+
+
 #CPU SPEED PRESETS
 #handled as strings here for sake of simplicity. 
 CPUSLOW="0.001"
@@ -68,32 +73,26 @@ CPUNORM="0.0005"
 CPUFAST="0.00001"
 CPUSUPER="0.000005"
 
-menulabel=simplefontC.render("Settings", True, (0, 0, 0), (255, 255, 255))
-setbg.blit(menulabel, (158, 4))
-menulabel=simplefontB.render("CPU Speed:", True, (0, 0, 0))
-setbg.blit(menulabel, (0, 62))
+cpu0=vmui.menuitem("Slow", "0.001")
+cpu1=vmui.menuitem("Normal", "0.0005")
+cpu2=vmui.menuitem("Fast", "0.00001")
+cpu3=vmui.menuitem("Super", "0.000005")
+cpumenu=[cpu0, cpu1, cpu2, cpu3]
+
 
 #frameskip selector code
 fskiplist=[0, 1, 2, 3, 4, 5, 10, 15, 20, 50, 100]
-fslen=10
-fspoint=0
-fscnt=0
-
+fskipmenu=list()
 for f in fskiplist:
-	if f == fskip:
-		fspoint=fscnt
-	fscnt += 1
+	dx=vmui.menuitem(str(f), f)
+	fskipmenu.extend([dx])
 
 #mixer rate selector code
 mixlist=[8000, 11025, 22050, 44100, 48000]
-mixlen=4
-mixpoint=0
-mixcnt=0
-
+mixmenu=list()
 for f in mixlist:
-	if f == mixrate:
-		mixpoint=mixcnt
-	mixcnt += 1
+	dx=vmui.menuitem(str(f), f)
+	mixmenu.extend([dx])
 
 qflg=0
 scupdate=1
@@ -102,77 +101,70 @@ while qflg==0:
 	if scupdate==1:
 		scupdate=0
 		screensurf.blit(setbg, (0, 0))
+		fmx=screensurf.blit(fvfilemenu, (3, 5))
+		mixx=screensurf.blit(mixbtn, (45, 5))
+		menulabel=catfont.render(str(mixrate), True, (0, 0, 0))
+		screensurf.blit(menulabel, (45, 5))
+		
+		fskipx=screensurf.blit(fskipbtn, (127, 5))
+		menulabel=catfont.render(str(fskip), True, (0, 0, 0))
+		screensurf.blit(menulabel, (127, 5))
+		
+		cpux=screensurf.blit(cpubtn, (209, 5))
 		if CPUWAIT==CPUSLOW:
-			CSX=screensurf.blit(cslowsel, (0, 80))
+			menulabel=catfont.render("Slow", True, (0, 0, 0))
+		elif CPUWAIT==CPUNORM:
+			menulabel=catfont.render("Normal", True, (0, 0, 0))
+		elif CPUWAIT==CPUFAST:
+			menulabel=catfont.render("Fast", True, (0, 0, 0))
+		elif CPUWAIT==CPUSUPER:
+			menulabel=catfont.render("Super", True, (0, 0, 0))
 		else:
-			CSX=screensurf.blit(cslow, (0, 80))
-		if CPUWAIT==CPUNORM:
-			CNX=screensurf.blit(cnormsel, (140, 80))
-		else:
-			CNX=screensurf.blit(cnorm, (140, 80))
-		if CPUWAIT==CPUFAST:
-			CFX=screensurf.blit(cfassel, (280, 80))
-		else:
-			CFX=screensurf.blit(cfas, (280, 80))
-		if CPUWAIT==CPUSUPER:
-			CUX=screensurf.blit(csupsel, (420, 80))
-		else:
-			CUX=screensurf.blit(csup, (420, 80))
-		quitx=screensurf.blit(quiticn, (550, 150))
-		savex=screensurf.blit(saveicn, (600, 150))
-		resetx=screensurf.blit(reseticn, (650, 150))
-		helpx=screensurf.blit(helpicn, (700, 150))
-		menulabel=simplefontB.render(("Frame Skip: " + str(fskip)), True, (0, 0, 0))
-		screensurf.blit(menulabel, (0, 120))
-		fsup=screensurf.blit(rbtn, (40, 140))
-		fsdown=screensurf.blit(lbtn, (0, 140))
-		menulabel=simplefontB.render(("Sound Mixer Rate: " + str(mixrate)), True, (0, 0, 0))
-		screensurf.blit(menulabel, (120, 120))
-		mixup=screensurf.blit(rbtn, (160, 140))
-		mixdown=screensurf.blit(lbtn, (120, 140))
+			menulabel=catfont.render("Other", True, (0, 0, 0))
+		screensurf.blit(menulabel, (209, 5))
 		
 		menulabel=simplefontB.render("Status Readouts.", True, (0, 0, 0))
-		screensurf.blit(menulabel, (0, 180))
+		screensurf.blit(menulabel, (0, 80))
 		if disablereadouts==0:
-			disre=screensurf.blit(swon, (0, 200))
+			disre=screensurf.blit(swon, (0, 100))
 		else:
-			disre=screensurf.blit(swoff, (0, 200))
+			disre=screensurf.blit(swoff, (0, 100))
 		menulabel=simplefontB.render("TTY (virtual text display)", True, (0, 0, 0))
-		screensurf.blit(menulabel, (250, 180))
+		screensurf.blit(menulabel, (250, 80))
 		if ttystyle==0:
-			ttyx=screensurf.blit(swon, (250, 200))
+			ttyx=screensurf.blit(swon, (250, 100))
 		else:
-			ttyx=screensurf.blit(swoff, (250, 200))
+			ttyx=screensurf.blit(swoff, (250, 100))
 		menulabel=simplefontB.render("Step By Step CPU debug mode.", True, (0, 0, 0))
-		screensurf.blit(menulabel, (0, 230))
+		screensurf.blit(menulabel, (0, 130))
 		if stepbystep==1:
-			stepx=screensurf.blit(swon, (0, 250))
+			stepx=screensurf.blit(swon, (0, 150))
 		else:
-			stepx=screensurf.blit(swoff, (0, 250))
+			stepx=screensurf.blit(swoff, (0, 150))
 		menulabel=simplefontB.render("Log execution process.", True, (0, 0, 0))
-		screensurf.blit(menulabel, (250, 230))
+		screensurf.blit(menulabel, (250, 130))
 		if vmexeclogflg==1:
-			vmex=screensurf.blit(swon, (250, 250))
+			vmex=screensurf.blit(swon, (250, 150))
 		else:
-			vmex=screensurf.blit(swoff, (250, 250))
+			vmex=screensurf.blit(swoff, (250, 150))
 		menulabel=simplefontB.render("Dump IObus on exit.", True, (0, 0, 0))
-		screensurf.blit(menulabel, (0, 280))
+		screensurf.blit(menulabel, (0, 180))
 		if logIOexit==1:
-			ioex=screensurf.blit(swon, (0, 300))
+			ioex=screensurf.blit(swon, (0, 200))
 		else:
-			ioex=screensurf.blit(swoff, (0, 300))
+			ioex=screensurf.blit(swoff, (0, 200))
 		menulabel=simplefontB.render("Dump memory bus on exit.", True, (0, 0, 0))
-		screensurf.blit(menulabel, (250, 280))
+		screensurf.blit(menulabel, (250, 180))
 		if logromexit==1:
-			romex=screensurf.blit(swon, (250, 300))
+			romex=screensurf.blit(swon, (250, 200))
 		else:
-			romex=screensurf.blit(swoff, (250, 300))
+			romex=screensurf.blit(swoff, (250, 200))
 		menulabel=simplefontB.render("Log memory bus operations", True, (0, 0, 0))
-		screensurf.blit(menulabel, (0, 330))
+		screensurf.blit(menulabel, (0, 230))
 		if tromlogging==1:
-			tromx=screensurf.blit(swon, (0, 350))
+			tromx=screensurf.blit(swon, (0, 250))
 		else:
-			tromx=screensurf.blit(swoff, (0, 350))
+			tromx=screensurf.blit(swoff, (0, 250))
 		
 		
 		
@@ -187,45 +179,69 @@ while qflg==0:
 			pygame.image.save(screensurf, (os.path.join('CAP', 'SCREENSHOT-settings.png')))
 			break
 		if event.type==MOUSEBUTTONDOWN:
-			if quitx.collidepoint(event.pos)==1 and event.button==1:
-				qflg=1
-				break
-			if helpx.collidepoint(event.pos)==1 and event.button==1:
-				subprocess.Popen(["python", "helpview.py", "settings.xml"])
-			if savex.collidepoint(event.pos)==1 and event.button==1:
-				libvmconf.saveconf()
-			if resetx.collidepoint(event.pos)==1 and event.button==1:
-				libvmconf.resetconf()
-				CPUWAIT=libvmconf.getconf("cpu", "cpuwait")
-				stepbystep=int(libvmconf.getconf("cpu", "stepbystep"))
-				vmexeclogflg=int(libvmconf.getconf("log", "vmexec"))
-				logromexit=int(libvmconf.getconf("log", "romexit"))
-				logIOexit=int(libvmconf.getconf("log", "ioexit"))
-				tromlogging=int(libvmconf.getconf("log", "tromlogging"))
-				disablereadouts=int(libvmconf.getconf("video", "disablereadouts"))
-				fskip=int(libvmconf.getconf("video", "fskip"))
-				ttystyle=int(libvmconf.getconf("video", "ttystyle"))
-				mixrate=int(libvmconf.getconf("audio", "mixrate"))
-				#frameskip selector code
-				fskiplist=[0, 1, 2, 3, 4, 5, 10, 15, 20, 50, 100]
-				fslen=10
-				fspoint=0
-				fscnt=0
-				for f in fskiplist:
-					if f == fskip:
-						fspoint=fscnt
-					fscnt += 1
-				#mixer rate selector code
-				mixlist=[8000, 11025, 22050, 44100, 48000]
-				mixlen=4
-				mixpoint=0
-				mixcnt=0			
-				for f in mixlist:
-					if f == mixrate:
-						mixpoint=mixcnt
-					mixcnt += 1
+			if fmx.collidepoint(event.pos)==1 and event.button==1:
+				menuret=vmui.menuset(filemenu, 3, 45, reclick=0, fontsize=26)
+				if menuret=="HELP":
+					subprocess.Popen(["python", "helpview.py", "settings.xml"])
+				if menuret=="SAVE":
+					libvmconf.saveconf()
+				if menuret=="RESET":
+					libvmconf.resetconf()
+					CPUWAIT=libvmconf.getconf("cpu", "cpuwait")
+					stepbystep=int(libvmconf.getconf("cpu", "stepbystep"))
+					vmexeclogflg=int(libvmconf.getconf("log", "vmexec"))
+					logromexit=int(libvmconf.getconf("log", "romexit"))
+					logIOexit=int(libvmconf.getconf("log", "ioexit"))
+					tromlogging=int(libvmconf.getconf("log", "tromlogging"))
+					disablereadouts=int(libvmconf.getconf("video", "disablereadouts"))
+					fskip=int(libvmconf.getconf("video", "fskip"))
+					ttystyle=int(libvmconf.getconf("video", "ttystyle"))
+					mixrate=int(libvmconf.getconf("audio", "mixrate"))
+					#frameskip selector code
+					print "dxreset"
+					scupdate=1
+				if menuret=="QUIT":
+					qflg=1
+					break
+			if mixx.collidepoint(event.pos)==1 and event.button==1:
+				mixratebak=mixrate
+				mixrate=vmui.menuset(mixmenu, 45, 45, reclick=0, fontsize=26)
+				if mixrate==None:
+					mixrate=mixratebak
 				scupdate=1
-				print "dxreset"
+			if cpux.collidepoint(event.pos)==1 and event.button==1:
+				cpubak=CPUWAIT
+				CPUWAIT=vmui.menuset(cpumenu, 209, 45, reclick=0, fontsize=26)
+				if CPUWAIT==None:
+					CPUWAIT=cpubak
+				scupdate=1
+			if fskipx.collidepoint(event.pos)==1 and event.button==1:
+				fskipbak=fskip
+				fskip=vmui.menuset(fskipmenu, 127, 45, reclick=0, fontsize=26)
+				if fskip==None:
+					fskip=fskipbak
+				scupdate=1
+			#if quitx.collidepoint(event.pos)==1 and event.button==1:
+				#qflg=1
+				#break
+			#if helpx.collidepoint(event.pos)==1 and event.button==1:
+				#subprocess.Popen(["python", "helpview.py", "settings.xml"])
+			#if savex.collidepoint(event.pos)==1 and event.button==1:
+				#libvmconf.saveconf()
+			#if resetx.collidepoint(event.pos)==1 and event.button==1:
+				#libvmconf.resetconf()
+				#CPUWAIT=libvmconf.getconf("cpu", "cpuwait")
+				#stepbystep=int(libvmconf.getconf("cpu", "stepbystep"))
+				#vmexeclogflg=int(libvmconf.getconf("log", "vmexec"))
+				#logromexit=int(libvmconf.getconf("log", "romexit"))
+				#logIOexit=int(libvmconf.getconf("log", "ioexit"))
+				#tromlogging=int(libvmconf.getconf("log", "tromlogging"))
+				#disablereadouts=int(libvmconf.getconf("video", "disablereadouts"))
+				#fskip=int(libvmconf.getconf("video", "fskip"))
+				#ttystyle=int(libvmconf.getconf("video", "ttystyle"))
+				#mixrate=int(libvmconf.getconf("audio", "mixrate"))
+				##frameskip selector code
+				#print "dxreset"
 			if disre.collidepoint(event.pos)==1 and event.button==1:
 				if disablereadouts==0:
 					disablereadouts=1
@@ -275,47 +291,4 @@ while qflg==0:
 					logIOexit=0
 				scupdate=1
 				libvmconf.setconf("log", "ioexit", str(logIOexit))
-			if CSX.collidepoint(event.pos)==1 and event.button==1:
-				CPUWAIT=CPUSLOW
-				libvmconf.setconf("cpu", "cpuwait", str(CPUWAIT))
-				scupdate=1
-			if CNX.collidepoint(event.pos)==1 and event.button==1:
-				CPUWAIT=CPUNORM
-				libvmconf.setconf("cpu", "cpuwait", str(CPUWAIT))
-				scupdate=1
-			if CFX.collidepoint(event.pos)==1 and event.button==1:
-				CPUWAIT=CPUFAST
-				libvmconf.setconf("cpu", "cpuwait", str(CPUWAIT))
-				scupdate=1
-			if  CUX.collidepoint(event.pos)==1 and event.button==1:
-				CPUWAIT=CPUSUPER
-				libvmconf.setconf("cpu", "cpuwait", str(CPUWAIT))
-				scupdate=1
-			if  fsup.collidepoint(event.pos)==1 and event.button==1:
-				fspoint += 1
-				if fspoint>fslen:
-					fspoint=0
-				fskip=fskiplist[fspoint]
-				libvmconf.setconf("video", "fskip", str(fskip))
-				scupdate=1
-			if  fsdown.collidepoint(event.pos)==1 and event.button==1:
-				fspoint -= 1
-				if fspoint<0:
-					fspoint=fslen
-				fskip=fskiplist[fspoint]
-				libvmconf.setconf("video", "fskip", str(fskip))
-				scupdate=1
-			if  mixup.collidepoint(event.pos)==1 and event.button==1:
-				mixpoint += 1
-				if mixpoint>mixlen:
-					mixpoint=0
-				mixrate=mixlist[mixpoint]
-				libvmconf.setconf("audio", "mixrate", str(mixrate))
-				scupdate=1
-			if  mixdown.collidepoint(event.pos)==1 and event.button==1:
-				mixpoint -= 1
-				if mixpoint<0:
-					mixpoint=mixlen
-				mixrate=mixlist[mixpoint]
-				libvmconf.setconf("audio", "mixrate", str(mixrate))
-				scupdate=1
+			
